f92fa56be0859b52bee5ad294ec379cb8e2d15bb
Ayushihelloworld <51502428+Ayushihelloworld@users.noreply.github.com>
2020-06-08 17:15:55 +0530

    Add files via upload

--- automation.py
+++ automation.py
@@ -1,23 +1,30 @@
+
+
+print(0)
 import selenium
 import sys
 import json
 import requests
-
+print(1)
 from urllib.parse import urlparse
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
+print(2)
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-
+print(3)
 browser = webdriver.Chrome(ChromeDriverManager().install() ,chrome_options = options)
 print("Yay baby!! ");
 
-def get_results(searchterm):
-	browser.get("https://www.google.com")
+# details = json.loads(sys.argv[:])
+# print(json.dumped(details))
 
+def get_results(searchterm):
+	print(4);
+	browser.get("https://www.google.com")
+	print("WHO ARE YOU??")
 	search_box  = browser.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
 	search_box.send_keys(searchterm)
 	search_box.submit()
@@ -54,7 +61,9 @@ def get_results(searchterm):
 	browser.close()
 	return
 
-Tommy  = get_results(json.loads(sys.stdin.readLines()[0]));
+print("hIIII");
+print(sys.argv[1:])
+Tommy  = get_results(json.loads(sys.argv[:]));
 
 print("Link to the desired Website: " + Tommy);
 # link = browser.find_element_by_link_text(Tommy)
--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const path = require("path");
+const {spawn} = require("child_process");
 // var PythonShell = require('python-shell');
 // var pyshell = new PythonShell('automation.py');
-var myPythonScript = "automation.py";
-var pythonExecutable = "python.exe";
+// let p = require('python-shell');
+// var myPythonScript = "automation.py";
 
+// import {PythonShell} from 'python-shell';
+// // var pythonExecutable = "python.exe";
+// let pyshell = new PythonShell('automation.py');
+//
+let {PythonShell} = require('python-shell');
 
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python3', ["automation.py"]);
+// /**
+//  * Run python script, pass in `-u` to not buffer console output
+//  * @return {ChildProcess}
+//  */
+// function runScript(){
+//   return spawn('python', [
+//     "-u",
+//     path.join(__dirname, 'automation.py'),
+//     "--foo", "some value for foo",
+//   ]);
+// }
 
+// const spawn = require("child_process").spawn;
+// const pythonProcess = spawn('python3', ["automation.py"]);
+// //
+// const subprocess = runScript()
 
 const app = express();
 app.use(express.static("public"));
@@ -17,7 +37,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
-
+  // var options = {
+  //         args:
+  //         [
+  //           JSON.stringify()''
+  //
+  //         ]
+  //       }
+  //       p.PythonShell.run('automation.py', options, function  (err, results)  {
+  //
+  //
+  //         console.log(results.toString())
+  //
+  //       });
 
 
 });
@@ -25,15 +57,30 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
   var searchQuery = req.body.searchquery;
   console.log(searchQuery);
-  var spawn = require("child_process").spawn;
-  const scriptExecution = spawn(pythonExecutable, ["automation.py", searchQuery]);
-  scriptExecution.stdout.on('data', (data) => {
-    console.log(uint8arrayToString(data));
-});
-  pyshell.end(function(err){
-  	if (err){
-  		throw err;
-  };
+  let pyshell = new PythonShell('automation.py');
+  var options = {
+          args:
+          [
+            JSON.stringify(searchQuery)
+
+          ]
+        }
+        console.log(options);
+        pyshell.send(options);
+        // p.PythonShell.run('automation.py', options, function  (err, results)  {
+        //   console.log("Hello!!");
+        //   console.log(results);
+        // //  console.log(results.toString())
+        //
+        // });
+        pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+          });
+          pyshell.end(function (err,code,signal) {
+  if (err) throw err;
+  console.log('The exit code was: ' + code);
+  console.log('The exit signal was: ' + signal);
   console.log('finished');
 });
  });
