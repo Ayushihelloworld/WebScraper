f903a9f1052bddfcf5297c5497479df793de74a4
Ayushihelloworld <51502428+Ayushihelloworld@users.noreply.github.com>
2020-06-10 15:57:33 +0530

    Finally stage 1 complete
    You can get link to any government based constitution pdf just be entering some info to the website :-)

--- automation.py
+++ automation.py
@@ -1,41 +1,64 @@
+
+
+print(0)
+import getopt
 import selenium
+import sys
+
+#print(sys.stdin.readLines())
+import json
+import requests
+
+
+print(1)
 from urllib.parse import urlparse
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
+print(2)
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
-
+print(3)
 browser = webdriver.Chrome(ChromeDriverManager().install() ,chrome_options = options)
+print("Yay baby!! ");
+print(sys.argv);
 
 def get_results(searchterm):
+	print(4);
 	browser.get("https://www.google.com")
-
+	print("WHO ARE YOU??")
 	search_box  = browser.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+	print("heyy")
+	print(browser.session_id)
+	searchterm = searchterm.pop()
 	search_box.send_keys(searchterm)
-	search_box.submit()
+	print(type(searchterm))
+	print(searchterm)
+
+	#search_box.submit()
+	print("A1");
 	links = browser.find_elements_by_tag_name("a");
-  
-	
+	print("A2")
+
 	results = []
 
 	for link in links:
+		print("A3")
 		href = link.get_attribute("href")
 		parsed = urlparse(href)
 	#	print(href)
-		print(parsed)
+	#	print(parsed)
 		CritA = parsed.netloc
-		print(type(CritA))
-		
+	#	print(type(CritA))
+
 		if (isinstance(CritA, type("ABD"))):
-			print("CRITA: " + CritA)	
+		#	print("CRITA: " + CritA)
 			if ("gov" in CritA):
 				CritB  = parsed.path
 				if ("pdf" in CritB):
 					aref = href
-					print("Aref: "  + aref);
+				#	print("Aref: "  + aref);
 					return aref;
 					break;
 		else:
@@ -45,9 +68,26 @@ def get_results(searchterm):
 					aref = href
 					return aref;
 					break;
-	
+
 	browser.close()
+	browser.quit();
 	return
 
-Tommy  = get_results("Constitution of India pdf")
-print("Tommy " + Tommy);
+print("hIIII");
+i = 0
+#print(get_results({"Constitution of USA"}))
+for line in sys.stdin:
+	print(line);
+	if i == 1 or 'Exit' == line.rstrip():
+		print('Sorry sir!! ')
+		break
+	else:
+		print(f'Processing Message from sys.stdin *****{line}*****')
+		print(get_results({line}))
+		print("Done");
+		i += 1;
+sys.stdout.flush();
+#print("Link to the desired Website: " + Tommy);
+sys.stdout.flush();
+# link = browser.find_element_by_link_text(Tommy)
+# link.click()
